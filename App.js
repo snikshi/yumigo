c43e5493f14d157cebb2f1849bfba158c69a8584
Shivarathri Nikhil <snikhil.nik@gmail.com>
2025-12-22 03:36:43 +0530

    Final Fix

--- App.js
+++ App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
 import { AuthProvider } from './src/context/AuthContext';
 import { CartProvider } from './src/context/CartContext';
-// REMOVED STRIPE FOR STABILITY 🛡️
+import { StripeProvider } from '@stripe/stripe-react-native';
 
 export default function App() {
   return (
-    <AuthProvider>
-      {/* 👇 Direct connection: No Stripe blocking the way */}
-      <CartProvider>
-          <AppNavigator />
-      </CartProvider>
-    </AuthProvider>
+    // 🔑 PASTE YOUR "pk_test_..." KEY HERE
+    <StripeProvider publishableKey="pk_test_51Sfknu08capLH0moi2dLDTq4rkwe352PjdsgNgTaXoQsVSM92Tx0r84OdK6S4mjY3za3v16Qewjaz0wHFIZPLSjV00NB6O2Kk2">
+      <AuthProvider>
+        <CartProvider>
+          <NavigationContainer>
+             <AppNavigator />
+          </NavigationContainer>
+        </CartProvider>
+      </AuthProvider>
+    </StripeProvider>
   );
 }
