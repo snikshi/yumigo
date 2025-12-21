a5600a75e907bb917a14cf82b0d5579494aa1a0b
Shivarathri Nikhil <snikhil.nik@gmail.com>
2025-12-22 03:52:10 +0530

    Final Fix

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native'; // 👈 THIS WAS MISSING
+import { StripeProvider } from '@stripe/stripe-react-native';
 import AppNavigator from './src/navigation/AppNavigator';
 import { AuthProvider } from './src/context/AuthContext';
 import { CartProvider } from './src/context/CartContext';
-import { StripeProvider } from '@stripe/stripe-react-native';
 
 export default function App() {
   return (
-    // 🔑 PASTE YOUR "pk_test_..." KEY HERE
+    // 🔑 REPLACE WITH YOUR "pk_test_..." STRIPE KEY
     <StripeProvider publishableKey="pk_test_51Sfknu08capLH0moi2dLDTq4rkwe352PjdsgNgTaXoQsVSM92Tx0r84OdK6S4mjY3za3v16Qewjaz0wHFIZPLSjV00NB6O2Kk2">
       <AuthProvider>
         <CartProvider>
